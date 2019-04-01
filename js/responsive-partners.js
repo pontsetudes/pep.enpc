d02ffc5126dfe9ff81be737fd78e93dfb8cf8525
Antoine <antoine.eudes@gmail.com>
2019-04-01 23:52:20 +0200

    finished partners, clients

---
+++ js/responsive-partners.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.customer-logos').slick({
+      autoplay: true,
+      autoplaySpeed: 1000,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      responsive: [{
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4
+        }
+      }, {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3
+        }
+      }]
+    });
+  });
