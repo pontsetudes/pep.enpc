68b932c6a2d810b933c19cb86cb02d91f3a2e88a
Antoine <antoine.eudes@gmail.com>
2019-04-03 22:38:52 +0200

    finished competences

---
+++ js/showtextonclick.js
@@ -0,0 +1,4 @@
+function showtextonclick(id_div_to_hide, id_div_to_show) {
+    document.getElementById(id_div_to_show).style.display = 'block';
+    document.getElementById(id_div_to_hide).style.display = 'none';
+}
