d078d6cb81140a9bb7e341ac13c0ec6f0af16999
Antoine <antoine.eudes@gmail.com>
2019-04-05 19:47:11 +0200

    finished contact form

--- js/contact.js
+++ js/contact.js
@@ -1,20 +1,45 @@
-// function display_contact_form() {
-//     document.getElementById("contact-form").style.display = 'block';
-//     document.getElementById("contact-form").style.backgroundColor = "white";
-// }
+function postToGoogleForm(form) {
+    formdata = new FormData(form);
+    $.ajax({
+        "async": true,
+        "crossDomain": true,
+        "url": "https://docs.google.com/forms/d/e/1FAIpQLSc1iQId4DG4Rejv8JVG2tmcXYpfztcGHAvuFwQryTbG5WZPzQ/formResponse",
+        "method": "POST",
+        "processData": false,
+        "contentType": false,
+        "mimeType": "multipart/form-data",
+        "data": formdata
+    });
+}
 
-// function check_empty() {
-//     if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
-//     alert("Fill All Fields !");
-//     } else {
-//     document.getElementById('form').submit();
-//     alert("Form Submitted Successfully...");
-//     }
-//     }
-//     //Function To Display Popup
-//     function div_show() {
-//     document.getElementById('abc').style.display = "block";
-//     }
-//     //Function to Hide Popup
-//     function div_hide(){
-//     document.getElementById('abc').style.display = "none";
+function validateForm(form) {
+
+    if (form.elements["entry.569483692"].value == "") {
+       return false;
+    }
+    if (form.elements["entry.683583938"].value == "") {
+       return false;
+    }
+    if (form.elements["entry.1746294369"].value == "") {
+       return false;
+    }
+    if (form.elements["entry.945021883"].value == "") {
+        return false;
+    }
+    if (form.elements["entry.226339160"].value == "") {
+        return false;
+    }
+    return true;
+ }
+
+function handleSubmitForm(form) {
+    document.activeElement.blur();
+    var isFormValid = validateForm(form);
+    if (!isFormValid) {
+        document.getElementById("form-error").style.display = 'block';
+    } else {
+        postToGoogleForm(form);
+        document.getElementById("form-error").style.display = 'none';
+        document.getElementById("form-success").style.display = 'block';
+    }
+}
